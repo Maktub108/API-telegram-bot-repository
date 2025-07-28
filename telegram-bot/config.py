6dd17f214d4698e3a5e53315a4494150ea05bda4
Maktub108 <e.kozulin@yandex.ru>
2025-07-28 10:41:32 +0500

    Create config.py

---
+++ telegram-bot/config.py
@@ -0,0 +1,14 @@
+
+TOKEN = '7974726104:AAFGdiRcY5dMYCdkYv0Zr2JjdSnSUVrf5jU'
+
+NASA_API_KEY = ('ENeD034LylxlJGbc8PbEVKU266lYzSkLQVhRHzki')
+EXCHANGE_RATE_API_KEY = ('EXCHANGE_RATE_API_KEY')
+POLYGON_API_KEY = ('JwtaD7lUJWX5igWMUTGfKfDWTuLtuV_g')
+
+
+
+NASA_APOD_URL = "https://api.nasa.gov/"
+EXCHANGE_RATE_URL = "https://www.blockchain.com/ru/explorer/api"
+POLYGON_BASE_URL = "https://polygon.io/docs/rest/stocks/overview"
+
+
